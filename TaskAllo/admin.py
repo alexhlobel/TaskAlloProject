44652dd9d8dd4f371b4b3aabb7161f101ece8fdf
Alexandr_Pankratov <pasaladr@gmail.com>
2022-07-24 00:53:20 +0300

    Installed django-dotenv, activated it. Put templates folder into comment, left only an emply list. Registered models in admin.py

--- TaskAllo/admin.py
+++ TaskAllo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(StatusEmployee)
+admin.site.register(Team)
+admin.site.register(Manager)
+admin.site.register(Managership)
+admin.site.register(Admin)
+admin.site.register(StatusTask)
+admin.site.register(Task)
--- TaskAllo/models.py
+++ TaskAllo/models.py
@@ -58,4 +58,3 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
-
