1255b06a06f25e805d1cabade47c8e58a43f12e5
Alexandr_Pankratov <pasaladr@gmail.com>
2022-07-24 22:18:41 +0300

    Messed up with migrations, restarted them from scratch, made it halfway through admin and models

--- TaskAllo/admin.py
+++ TaskAllo/admin.py
@@ -1,11 +1,145 @@
 from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
 from .models import *
 
+print(User.__dict__.keys())
+print(models.ImageField.__dict__.keys())
+'''
+    date_hierarchy = 'created_at'
+    list_display = ('title', 'created_at', 'preview_content', 'author', 'preview_image')
+    list_filter = ('created_at', 'author')
+    search_fields = ('title', 'content', 'author__username')
+    readonly_fields = ('created_at', 'created_at')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'content', 'author', 'image')
+        }),
+        ('Дополнительная информация', {
+            'classes': ('collapse',),
+            'fields': ('created_at',),
+        }),
+    )
 
-admin.site.register(StatusEmployee)
-admin.site.register(Team)
-admin.site.register(Manager)
-admin.site.register(Managership)
-admin.site.register(Admin)
-admin.site.register(StatusTask)
-admin.site.register(Task)
+    @staticmethod
+    def preview_image(obj):
+        if obj.image:
+            print(obj.image.url)
+            return mark_safe(f'<img src="{obj.image.url}" width="100" />')
+            # return mark_safe(f'<img src="../media/ImagePost/pikachu.png" width="100" />')
+
+    @staticmethod
+    def preview_content(obj):
+        return obj.content[:30]
+'''
+
+
+class NameAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    list_display = ['name']
+    search_fields = ['name']
+
+
+class StatusEmployeeAdmin(NameAdmin):
+    pass
+
+
+class TeamAdmin(NameAdmin):
+    pass
+
+
+class WorkerAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_joined'
+    ordering = ['username']
+    list_display = ['username', 'id', 'status_emp', 'team', 'last_login', 'is_superuser',  'first_name', 'last_name',
+                    'email', 'is_staff', 'is_active', 'date_joined', ]
+    list_filter = ['status_emp', 'team']
+    search_fields = ['username', 'email']
+    readonly_fields = ('date_joined', 'last_login', 'id')
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'id', 'status_emp', 'team', 'first_name',
+                       'last_name', 'date_joined', 'password', )
+        }),
+        ('Additional parameters', {
+            'classes': ('collapse',),
+            'fields': ('last_login', 'is_superuser', 'is_staff', 'is_active'),
+        }),
+    )
+
+
+class ManagerAdmin(admin.ModelAdmin):
+
+    @staticmethod
+    def get_teams(obj):
+        return ",\n".join([str(p) for p in obj.team.all()])
+
+        # Когда потом в get_teams передается obj? Почему это срабатывает?
+
+    date_hierarchy = 'date_joined'
+    ordering = ['username']
+    list_display = ['username', 'id', 'status_emp', 'get_teams', 'last_login',
+                    'is_superuser',  'first_name', 'last_name', 'email',
+                    'is_staff', 'is_active', 'date_joined', ]
+    list_filter = ['status_emp', 'team']
+    search_fields = ['username', 'email']
+    readonly_fields = ('date_joined', 'last_login', 'id', 'team')
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'id', 'status_emp', 'team', 'first_name',
+                       'last_name', 'date_joined', 'password', )
+        }),
+        ('Additional parameters', {
+            'classes': ('collapse',),
+            'fields': ('last_login', 'is_superuser', 'is_staff', 'is_active'),
+        }),
+    )
+
+
+class ManagershipAdmin(admin.ModelAdmin):
+    fields = ['manager', 'team', 'id']
+    ordering = ['manager', 'id']
+    list_display = ['manager', 'team', 'id']
+    search_fields = ['name', 'team']
+    list_filter = ['manager', 'team']
+    readonly_fields = ['id']
+
+
+class AdminAdmin(ManagerAdmin):
+    pass
+
+
+class StatusTaskAdmin(NameAdmin):
+    pass
+
+
+class TaskAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    ordering = ['name']
+    list_display = ['name', 'id', 'description', 'team', 'created_at', 'updated_at',
+                    'author', 'status_task', 'deadline', ]
+    list_filter = ['team', 'author']
+    search_fields = ['username', 'email']
+    readonly_fields = ('created_at', 'updated_at', 'id', 'connection')
+    fieldsets = (
+        (None, {
+            'fields': ['name', 'id', 'description', 'team', 'created_at', 'updated_at',
+                       'author', 'status_task', 'deadline', 'connection']
+        }),
+        ('Additional parameters', {
+            'classes': ('collapse',),
+            'fields': (),
+        }),
+    )
+
+
+admin.site.register(StatusEmployee, StatusEmployeeAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Worker, WorkerAdmin)
+admin.site.register(Manager, ManagerAdmin)
+admin.site.register(Managership, ManagershipAdmin)
+admin.site.register(Admin, AdminAdmin)
+admin.site.register(StatusTask, StatusTaskAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment)
+admin.site.register(Image)
+admin.site.register(ImageSource)
--- TaskAllo/models.py
+++ TaskAllo/models.py
@@ -5,10 +5,19 @@ from django.contrib.auth.models import User
 class StatusEmployee(models.Model):
     name = models.TextField(max_length=120)
 
+    class Meta:
+        verbose_name = 'Status Employee'
+        verbose_name_plural = 'Statuses Employee'
+
+    def __str__(self):
+        return self.name
+
 
 class Employee(User):
     class Meta:
-        abstract = True
+        # abstract = True
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
 
     status_emp = models.ForeignKey(StatusEmployee, on_delete=models.CASCADE)
 
@@ -17,27 +26,67 @@ class Employee(User):
 
 
 class Team(models.Model):
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
     name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
 
 
 class Worker(Employee):
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='worker_team')
+    class Meta:
+        verbose_name = 'Worker'
+        verbose_name_plural = 'Workers'
+
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='worker_team')
+
+    def __str__(self):
+        return self.username
 
 
 class Manager(Employee):
+    class Meta:
+        verbose_name = 'Manager'
+        verbose_name_plural = 'Managers'
+
     team = models.ManyToManyField(Team, through='Managership', related_name='manager_team')
+
+    def __str__(self):
+        return self.username
 
 
 class Managership(models.Model):
-    team = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, related_name='manship_team')
-    manager = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='manship_manager')
+    class Meta:
+        verbose_name = 'Managership'
+        verbose_name_plural = 'Managerships'
+
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name='manship_team')
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True,
+                                related_name='manship_manager')
+
+    def __str__(self):
+        return f'{self.manager}' + '_' + f'{self.id}'
 
 
 class Admin(Manager):
+    class Meta:
+        verbose_name = 'Admin'
+        verbose_name_plural = 'Admins'
+
     pass
+
+    def __str__(self):
+        return self.username
 
 
 class StatusTask(models.Model):
+    class Meta:
+        verbose_name = 'Status Task'
+        verbose_name_plural = 'Status Tasks'
+
     name = models.TextField(max_length=120)
 
     def __str__(self):
@@ -45,12 +94,16 @@ class StatusTask(models.Model):
 
 
 class Task(models.Model):
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
     name = models.TextField(max_length=120, null=False, blank=False)
     description = models.TextField()
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='task_team')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='Image', null=True, blank=True)
+    # image_source_id = models.OneToOneField(ImageSource, on_delete=models.SET_NULL, null=True, blank=True)
     author = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, related_name='task_author')
     status_task = models.ForeignKey(StatusTask, on_delete=models.SET_NULL, null=True, related_name='status_task')
     deadline = models.DateTimeField(null=True, blank=True)
@@ -58,3 +111,28 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+
+class ImageSource(models.Model):
+    class Meta:
+        verbose_name = 'Image Source'
+        verbose_name_plural = 'Image Sources'
+
+    comment = models.OneToOneField(Comment, on_delete=models.CASCADE, related_name='source_comment')
+    task = models.OneToOneField(Comment, on_delete=models.CASCADE, related_name='source_task')
+
+
+class Image(models.Model):
+    class Meta:
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
+
+    image = models.ImageField(upload_to='Image')
+    source = models.ForeignKey(ImageSource, on_delete=models.CASCADE)
