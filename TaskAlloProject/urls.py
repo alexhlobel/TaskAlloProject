bd9ac2d0f82b25f26cb5cf89e42ed27047393e71
Alexandr_Pankratov <pasaladr@gmail.com>
2022-07-25 15:04:19 +0300

    urls and views by ValPirate

--- TaskAlloProject/urls.py
+++ TaskAlloProject/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('main/', include('TaskAllo.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
